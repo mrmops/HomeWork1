2de57c8810659ce2c4824a69b64b008d0576ba45
kirie <kiriell.marshall@gmail.com>
2021-03-04 00:13:25 +0500

    fix

--- app/src/main/java/com/example/homework1/SquareActivity.kt
+++ app/src/main/java/com/example/homework1/SquareActivity.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 
 class SquareActivity : AppCompatActivity() {
 
     companion object{
+        val LOG_KEY = SquareActivity::class.java.simpleName
+
         const val SQUARE_NUMBER_KEY = "Number key"
 
         fun newIntent(context: Context, number: Int): Intent{
@@ -23,10 +26,17 @@ class SquareActivity : AppCompatActivity() {
     var number = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(LOG_KEY, "On create")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_square)
         squareTextView = findViewById(R.id.square_text_view)
-        number = intent.getIntExtra(SQUARE_NUMBER_KEY, 0)
-        squareTextView.text = (number * number).toString()
+        val extras = intent.extras
+        if(extras != null) {
+            number = extras.getInt(SQUARE_NUMBER_KEY)
+            squareTextView.text = (number * number).toString()
+        }
+        else{
+            throw IllegalArgumentException()
+        }
     }
 }
